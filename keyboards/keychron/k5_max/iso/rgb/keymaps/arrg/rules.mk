aad7dc4f398a2713b1b87922c9fbd5d52b65d556
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2025-01-02 11:15:56 +0000

    init custom changes

---
+++ keyboards/keychron/k5_max/iso/rgb/keymaps/arrg/rules.mk
@@ -0,0 +1,33 @@
+VIA_ENABLE = yes
+
+
+# DYNAMIC_MACRO_ENABLE = yes
+# DM_REC1
+# DM_REC2
+# DM_PLY1
+# DM_PLY2
+
+# https://docs.qmk.fm/features/autocorrect#how-do-i-enable-autocorrection
+# AUTOCORRECT_ENABLE = yes
+
+# https://docs.qmk.fm/features/leader_key
+# LEADER_ENABLE = yes
+
+# AUTO_SHIFT_ENABLE = yes
+# REPEAT_KEY_ENABLE = yes
+
+# MOUSEKEY_ENABLE = yes
+
+# CAPS_WORD_ENABLE = yes
+
+# https://docs.qmk.fm/features/rgb_matrix#custom-rgb-matrix-effects
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_CUSTOM_USER = yes
+
+
+# EXTRAKEY_ENABLE = no # Audio control and System control
+# LK_WIRELESS_ENABLE = no
+
+CONSOLE_ENABLE = yes
+# MOUSEKEY_ENABLE = no
+# NKRO_ENABLE = no
