9c08ba2590ffd1c73d9ec43c20cc8d6546cf236c
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2025-06-06 20:17:45 +0100

    fix alt

--- keyboards/keychron/k5_max/iso/rgb/keymaps/arrg/keymap.c
+++ keyboards/keychron/k5_max/iso/rgb/keymaps/arrg/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,
         _______,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,                               KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,             KC_P1,   KC_P2,   KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_RALT,                         LT(SPACE_FN, KC_SPC),                          KC_RALT,  KC_RWIN,  MO(WIN_FN),QK_LOCK, KC_LEFT, KC_DOWN,  KC_RGHT, KC_P0,            KC_PDOT, KC_PENT),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                         LT(SPACE_FN, KC_SPC),                          KC_RALT,  KC_RWIN,  MO(WIN_FN),QK_LOCK, KC_LEFT, KC_DOWN,  KC_RGHT, KC_P0,            KC_PDOT, KC_PENT),
 
     /*
         Gaming Layer
